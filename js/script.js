1baa3d80e425a30bea4e8a030690d10f96e1d3b4
tahakyo <ghazouani1988@email.com>
2022-04-26 18:43:39 +0200

    rifatto l'esercizio da capo con l'aiuto della correzzione

--- js/script.js
+++ js/script.js
@@ -3,44 +3,62 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 
-
-// al click del buttone play visualizzo la griglia
-document.getElementById("play").addEventListener("click", function() {
-    // Genero numeri da 1 -->100 
-    const gridSize = 100;
-    const gridArray = generateGridNumber(gridSize);
-    // per ogni numero creo un grid-item
-    const gridContainer = document.querySelector(".grid-container");
-    for (let i = 0; i < gridArray.length; i++) {
-    // creo un elemento html
-    const newElement = document.createElement('div')
-
-    // creo lo span con il numero 
-    newElement.innerHTML = `<span>${gridArray[i]}</span>`
-    // assegno la classe grid-item
-    newElement.classList.add("grid-item");
-    // aggiungo all'elemento appena creato la gestione del click
-    newElement.addEventListener("click", function() {
-        this.classList.add("active");
-    })
-    // appendo questo elemento al contenitore
-    gridContainer.append(newElement);
-
-}
-
-})
+// Al click sul bottone Play far partire il gioco
+const playButton = document.getElementById("play-btn");
+playButton.addEventListener("click", startGame);
 
 
-// functions
-
-function generateGridNumber(gridNumberQuantity) {
-    // creo un array per generarci dentro il numero di celle corrispondente al grid size
-    const arrayNumber = [];
-    // genero i numeri corrsipondenti al grid size
-    for (let i = 1; i <= gridNumberQuantity; i++) {
-        const cellNumber = i;
-    // faccio il push dei numeri generati dentro l array
-        arrayNumber.push(cellNumber);
+// Main function
+function startGame() {
+    // Nascondere la scritta con classe "hidden"
+    // Far vedere il contenitore della griglia, togliendo la classe "hidden"
+    const title = document.getElementById("title");
+    const grid = document.getElementById("grid");
+    title.classList.add("hidden");
+    grid.classList.remove("hidden");
+    grid.innerHTML = "";
+    // Prelevare la scelta della difficoltà dell'utente
+    const difficulty = parseInt(document.getElementById('level').value);
+    let cellNumber = 0;
+    let cellNumberInRow = 0;
+    if (difficulty === 1) {
+        cellNumber = 100;
+        cellNumberInRow = 10;
+    } else if (difficulty === 2) {
+        cellNumber = 81;
+        cellNumberInRow = 9;
+    } else {
+        cellNumber = 49;
+        cellNumberInRow = 7;
     }
-    return arrayNumber;
+    // generare le celle da 1 a 100
+    for (let i = 1; i <= cellNumber; i++) {
+        const newItem = generateGridItem(i, cellNumberInRow);
+        // aggiungo la function cell clicked
+        newItem.addEventListener("click", cellClicked);
+        // appendere la cella generata al contenitore
+        grid.append(newItem);
+
+    }
 }
+
+// creo una function che colora di blu la cella quando viene cliccata
+function cellClicked() {
+    this.classList.add("clicked");
+}
+
+// creare una function che genera gli items della grid
+function generateGridItem(gridNumber, cellsInRow) {
+    // creare l'elemento html
+    const gridItem = document.createElement("div");
+    // aggiungere la classe grid-item
+    gridItem.classList.add("grid-item");
+    // settare le dimensioni delle celle corrispondenti
+    gridItem.style.width = `calc(100% / ${cellsInRow})`;
+    gridItem.style.height = `calc(100% / ${cellsInRow})`;
+    // inserire lo span con il numero di cella corrispondente
+    gridItem.innerHTML = `<span>${gridNumber}</span>`;
+    return gridItem;
+}
+
+
